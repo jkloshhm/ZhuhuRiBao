341ec1023c805a256c7dd8e299acce6a2d622501
jkloshhm <jkloshhm@126.com>
2019-05-28 23:59:34 +0800

    依赖包更新到AndroidX。

--- app/src/main/java/com/example/guojian/zhuhuribao/MainActivity.java
+++ app/src/main/java/com/example/guojian/zhuhuribao/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.guojian.zhuhuribao;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
